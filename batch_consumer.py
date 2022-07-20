0df3a55b3fdaa9adb338673b30c12a9262c5e00b
Emma Samouelle <emma.samouelle@gmail.com>
2022-07-20 16:49:23 +0100

    Adds method to upload batch to S3

--- batch_consumer.py
+++ batch_consumer.py
@@ -1,42 +1,72 @@
-from kafka import KafkaConsumer
-from json import loads
-import time
 import boto3
 import botocore
-from botocore.exceptions import ClientError
 import json
-import schedule
 import os
-import tempfile
+import shutil
+from json import loads
+from kafka import KafkaConsumer
 
 s3 = boto3.client('s3')
 bucket = 'pintaic'
 path = os.getcwd()
+dir_path = path + '/bucket'
 
-# create our consumer to retrieve the message from the topics
 batch_consumer = KafkaConsumer(
     bootstrap_servers="localhost:9092",    
-    value_deserializer=lambda message: loads(message), # turns into a dictionary 
+    value_deserializer=lambda message: loads(message), 
     auto_offset_reset="earliest"
 )
 
 batch_consumer.subscribe(topics=["PinterestTopic"])
-
 batch_consumer.poll()
 
-i = 0
+# i = 0
+# for fact in batch_consumer:
+#     timestamp = fact.timestamp
+#     file_name = f'{timestamp}.json'
+#     with open(os.path.join(dir_path, file_name), 'w') as f:
+#         json.dump(fact, f)
+#     # s3.upload_file(file_name, bucket, file_name)
+#     i += 1
+#     if i == 10:
+#         break
 
-for message in batch_consumer:
-    timestamp = message.timestamp
-    dir_path = path + 'data'
-    os.makedirs(dir_path)
-    with open(os.path.join(dir_path, f'{timestamp}.json'), 'w') as f:
-        json.dump(message, f, ensure_ascii=True)
-    i += 1
-    if i == 10:
-        break
+# for (root, dirs, files) in os.walk(dir_path):
+#     for file in files:
+#         s3.upload_file(os.path.join(root,file),bucket,file)
+# shutil.rmtree(dir_path)
 
+def batch_to_directory(consumer : str , dir_path : str, number_facts : int):
+    '''
+    Consumes set number of facts/messages at once and saves in a directory with unique timestamp id 
+    Args:
+        consumer: kafka consumer name
+        dir_path: directory path for consumer data
+        number_facts: number of messages in this batch
+    
+    '''
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    i = 0
+    for fact in batch_consumer:
+        timestamp = fact.timestamp
+        file_name = f'{timestamp}.json'
+        with open(os.path.join(dir_path, file_name), 'w') as f:
+            json.dump(fact, f)
+        i += 1
+        if i == number_facts:
+            break
 
-# with open('batch .json', 'w') as write_file:
-#     json.dump(batch, write_file, ensure_ascii=True)
-# response = s3.upload_file('batch.json', bucket)
+def upload_dir_S3(dir_path : str):
+    '''
+    Uploads directory to S3 then deletes
+    Args: dir_path: path to directory to be uploaded
+    '''
+    for (root, dirs, files) in os.walk(dir_path):
+        for file in files:
+            result = s3.upload_file(os.path.join(root,file),bucket,file)
+            if result:
+                shutil.rmtree(dir_path)
+
+batch_to_directory(consumer=batch_consumer , dir_path=dir_path, number_facts=10)
+upload_dir_S3(dir_path=dir_path)
